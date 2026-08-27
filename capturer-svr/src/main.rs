e023990e09e77a7e40b59390a5d7cd459652fd30
nnzbz <nnzbz@qq.com>
2026-08-27 08:32:01 +0800

    * 优化变量名

--- capturer-svr/src/main.rs
+++ capturer-svr/src/main.rs
@@ -59,7 +59,7 @@ async fn main() -> anyhow::Result<()> {
     // 解析命令行参数
     let Args {
         signal,
-        config_file,
+        config_file: config_file_path,
         port,
     } = Args::parse();
 
@@ -72,7 +72,7 @@ async fn main() -> anyhow::Result<()> {
     let (mut signal_manager, old_pid) = SignalManager::new(signal)?;
 
     let app_watcher: AppWatcher<AppConfig> = AppWatcher::new(
-        config_file,
+        config_file_path,
         log_watcher.config_changed_tx.clone(),
         move |app_config: Arc<AppConfig>| async move {
             // 更新capturer的配置
